dfb3c6f0cd50560216e106ebba461e993421ffb5
SamIfesi <Ekesamrose@gmail.com>
2026-08-14 16:06:05 +0100

    feat: add remotePatterns for images in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.1.151', 'localhost'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ghchart.rshah.org',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
