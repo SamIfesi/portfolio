035ef50049b05ab574172124cccbbcc89749999f
SamIfesi <Ekesamrose@gmail.com>
2026-08-25 19:39:26 +0100

    feat: add Skills section to Home component for improved showcase of skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import { Nav } from "@/components/nav";
-import { Hero } from "@/components/sections/hero";
-import { Projects } from "@/components/sections/projects";
-import { About } from "@/components/sections/about";
-import { Contact } from "@/components/sections/contact";
+import { Nav } from '@/components/nav';
+import { Hero } from '@/components/sections/hero';
+import { Skills } from '@/components/sections/skills';
+import { Projects } from '@/components/sections/projects';
+import { About } from '@/components/sections/about';
+import { Contact } from '@/components/sections/contact';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Nav />
       <Hero />
       <About />
+      <Skills />
       <Projects />
       <Contact />
     </main>
